492ae8594e92213a5431e0167c79382be214c2f6
Vladislav Samura <vx5080@yandex.ru>
2023-10-04 00:24:34 +0900

    Added the second exercise

--- script.js
+++ script.js
@@ -32,13 +32,18 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-
-let oneOfTheFilms = prompt("Один из последних просмотренных фильмов?", "");
-let аssessmentOfTheFilms = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[oneOfTheFilms] = аssessmentOfTheFilms;
-
-oneOfTheFilms = prompt("Один из последних просмотренных фильмов?", "");
-аssessmentOfTheFilms = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[oneOfTheFilms] = аssessmentOfTheFilms;
-
+for (let i = 0; i<2 ; i++){
+    let a = prompt("Один из последних просмотренных фильмов?", "");
+    let b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 ){
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    }
+    else{
+        console.log("error");
+        i--;
+    };   
+}
 console.log(personalMovieDB);
+
+// console.log(personalMovieDB);
