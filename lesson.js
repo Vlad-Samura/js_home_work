440c514f38c970339dc0677ca2a61cca70017583
Vladislav Samura <vx5080@yandex.ru>
2023-10-05 22:17:42 +0900

    Added exercises in a separate file

---
+++ lesson.js
@@ -0,0 +1,20 @@
+function sayHello(name) {
+    return "Привет, " + name;
+}
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+
+// Место для третьей задачи
+function getMathResult(num, repeat) {
+    if (repeat <= 1 || typeof(repeat) != "number") return num;
+    let result = "";
+    for(let i = 1; i <= repeat; i++){
+        result += num * i;
+        if(i != repeat) result += "---";
+    }
+    return result;
+}
+console.log(getMathResult(5, 4));
