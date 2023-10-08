58a6705019a42482a563b3036b0d9087d29fc98b
Vladislav Samura <vx5080@ya.ru>
2023-10-08 22:25:31 +0900

    Intermediate fix

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        let a = prompt("Один из последних просмотренных фильмов?", "");
-        let b = prompt("На сколько оцените его?", "");
+        let a = prompt("Один из последних просмотренных фильмов?", "").trim;
+        let b = prompt("На сколько оцените его?", "").trim;
         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log("done");
