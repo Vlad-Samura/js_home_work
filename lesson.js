7461337244b40d291b2ce1ff65916ae4335039ba
Vladislav Samura <vx5080@ya.ru>
2023-10-09 22:40:46 +0900

    Added tasks and solutions to them

--- lesson.js
+++ lesson.js
@@ -122,3 +122,143 @@ function fib(a) {
 }
 
 
+//******************************************************************************/
+// Задачи:
+// 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+// Пример:
+// showExperience(personalPlanPeter) => '1 month'
+// P.S. желательно использовать деструктуризацию, но не обязательно
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+// Пример:
+// showProgrammingLangs(personalPlanPeter)  =>
+// "Язык js изучен на 20% Язык php изучен на 10%"
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+// P.S. Для переноса строки используется \n в конце строки.
+// 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+// Пример:
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan){
+        let leng = (plan.skills.languages);
+        leng = leng.join(" ").toUpperCase(); 
+        return `Мне ${plan.age} и я владею языками: ${leng}`;
+    }
+};
+
+function showExperience(plan) {
+    return plan.skills.exp;
+}
+
+function showProgrammingLangs(plan) {
+    let result = "";
+    for (let val in plan.skills.programmingLangs){
+        result += `Язык ${val} изучен на ${plan.skills.programmingLangs[val]}\n`;
+    }
+    result.trim;
+    return result;
+}
+
+
+//******************************************************************************/
+// Задачи:
+// 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+// 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+// Пример:
+// standardizeStrings(favoriteCities)  выведет в консоль
+// lisbon
+// rome
+// milan
+// dublin
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let result = ""
+    for(let i of arr){
+        result += " " + i;
+    }
+    result.trim;
+    return result ? `Семья состоит из:${result}` : "Семья пуста";
+}
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    for(let i of arr){
+        console.log(i.toLowerCase());
+    }
+}
+
+// standardizeStrings(favoriteCities);
+// console.log(showFamily(family));
+
+
+//******************************************************************************/
+// Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+// Пример:
+// const someString = 'This is some strange string';
+// reverse(someString) => 'gnirts egnarts emos si sihT'
+// Функцию можно применить к любой строке. Если в функцию приходит не строка - вернуть сообщение "Ошибка!"
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) != "string") return "Ошибка!";
+    let result = "";
+    for( let i = str.length - 1; i >= 0 ; i--){
+        result += str[i]
+    }
+    return result;
+}
+
+//console.log(reverse(someString));
+
+
+//******************************************************************************/
+// Задача
+// Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+// Пример:
+// availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+// Вернет строку:
+// Доступные валюты:
+// UAH
+// RUB
+// Заметьте:
+// - CNY (юань) исчез из списка валют, значит такая валюта закончилась
+// - После валюты: стоит перенос строки \n, и после каждой валюты тоже. 
+// - Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    if (arr.length == 0) return "Нет доступных валют";
+    let result = "Доступные валюты:\n";
+    for (let value of arr){
+        if(value == missingCurr) continue;
+        result += value + "\n";
+    }
+    return result;
+}
+
+//console.log(availableCurr(['UAH', 'RUB', 'CNY'], 'CNY'));
+
+//******************************************************************************/
